f0c5e7be3bb9c7171cd425cb63385dc26c41f15e
Artem <mrartemtitenko@gmail.com>
2022-01-21 20:03:03 +0200

    firstcommit

--- ConsoleApplicationOOP/WorkClass.cpp
+++ ConsoleApplicationOOP/WorkClass.cpp
@@ -4,11 +4,7 @@ void WorkClass::startWork() {
 	Freezer freezer01(1, 234, "Freezer Samsung", "Good thing...", 2, 34);
 	Freezer* pointerFreezer01 = &freezer01;
 
-    GoodBasic* universalPointer01 = new GoodBasic;
-    GoodBasic* universalPointer02 = new Freezer(1, 234, "Freezer Samsung", "Good thing...", 2, 34);
-    GoodBasic* universalPointer03 = new Software(1, 234, "Soft FOR Samsung", "Very Good thing...", 2, 34);
+	GoodBasic* universalPointer02 = new Freezer;
+	static_cast<Freezer*>(universalPointer02)->toString();
 
-    static_cast<GoodBasic*>(universalPointer01)->toString();
-    static_cast<GoodBasic*>(universalPointer02)->toString();
-    static_cast<GoodBasic*>(universalPointer03)->toString();
 }
